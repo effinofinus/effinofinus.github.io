bedb808dac1088f34470f5b03355bc57255445b5
effinofinus <git@theticehurstballards.co.uk>
2021-08-06 13:39:23 +0100

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-625JTFZ9TW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-625JTFZ9TW');
+</script>
